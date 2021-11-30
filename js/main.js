7843505f07609086589600ed762849d2dd19242a
Anusha SP <anushaspga@gmail.com>
2021-11-30 17:03:13 +0530

    banana changes

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputdiv = document.querySelector("#output");
+
+//var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+btnTranslate.addEventListener("click", clickHandler);
+
+function clickHandler(){
+    //  outputdiv.innerText = "ajsjsjsjsjsjsj "+txtInput.value;
+    var inputTxt = txtInput.value;
+    fetch(getTranslationUrl(inputTxt))
+    .then(respone => respone.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputdiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+  };
+
+function getTranslationUrl(input){
+    return serverUrl+ "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("error ",error);
+    alert("something wrong with the server! try again");
+}
+
+
